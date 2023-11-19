b6933da2ecad55fcc6eb8a70f61a7e9b0d65ac3f
Galo Gamboa <galin_71@hotmail.com>
2023-11-19 07:02:54 +0100

    Créer un serveur local

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('funciona');
+// Apeler express 
+
+/*    Vous devriez utiliser require, 
+      mais comme dans package.json j'incrémente
+      "type": "module", je vais utiliser import
+*/
+
+import express from 'express';
+
+// Créer l'application:
+const app = express();
+
+
+// Écoutez le serveur:
+const PORT = 3000;
+app.listen(PORT, ()=>{
+   console.log(`Le serveur est en écoute sur : http://localhost:${PORT}`)
+})
+
+
